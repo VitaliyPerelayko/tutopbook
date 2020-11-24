296a177711dd460b202e6380d11c723670e83ee4
vital <ant-1989alo@mail.ru>
2020-11-24 14:45:49 +0300

    initial commit

---
+++ src/main/java/com/tob/tutorbook/TutorbookApplication.java
@@ -0,0 +1,13 @@
+package com.tob.tutorbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tob/tutorbook/TutorbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tob.tutorbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
